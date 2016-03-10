32f3b18a30d1bd3d9af368afbaa9b055bea5166f
Lixing <zlxjackie@hotmail.com>
2016-03-10 21:47:17 +1100

    Add recommended tutor controller

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -1,41 +1,45 @@
 'use strict';
 
 module.exports = ['$scope', 'TutorApiService', 'toastr', '$http', '$state',
-  function($scope, TutorApiService, toastr, $http, $state){
+  function ($scope, TutorApiService, toastr, $http, $state) {
 
-  // Any function returning a promise object can be used to load values asynchronously
-  $scope.getLocations = function(location) {
-    return TutorApiService.getLocations(location).then(function(locations) {
-      return locations;
-    })
-  };
+    // Any function returning a promise object can be used to load values asynchronously
+    $scope.getLocations = function (location) {
+      return TutorApiService.getLocations(location).then(function (locations) {
+        return locations;
+      })
+    };
 
-  $scope.getSubjects = function(subject) {
-    return TutorApiService.getSubjects(subject).then(function(subjects) {
-      return subjects
-    })
-  };
+    $scope.getSubjects = function (subject) {
+      return TutorApiService.getSubjects(subject).then(function (subjects) {
+        return subjects
+      })
+    };
 
-  $scope.searchTutors = function() {
-    var query = {};
+    $scope.searchTutors = function () {
+      var query = {};
 
-    if (angular.isDefined($scope.location)) {
-      if (angular.isObject($scope.location)) {
-        query.locationid = $scope.location.id;
-      } else {
-        query.location = $scope.location;
+      if (angular.isDefined($scope.location)) {
+        if (angular.isObject($scope.location)) {
+          query.locationid = $scope.location.id;
+        } else {
+          query.location = $scope.location;
+        }
       }
-    }
 
-    if (angular.isDefined($scope.subject)) {
-      if (angular.isObject($scope.subject)) {
-        query.subjectids = $scope.subject.id;
-      } else {
-        query.keywords = $scope.subject;
+      if (angular.isDefined($scope.subject)) {
+        if (angular.isObject($scope.subject)) {
+          query.subjectids = $scope.subject.id;
+        } else {
+          query.keywords = $scope.subject;
+        }
       }
-    }
-    
-    $state.go('tutors', query);
-  }
 
-}];
+      $state.go('tutors', query);
+    };
+
+    TutorApiService.getRecommendedTutors().then(function (recommendedTutors) {
+      $scope.recommendedTutors = recommendedTutors;
+    });
+
+  }];
