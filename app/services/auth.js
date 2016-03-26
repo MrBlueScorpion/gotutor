05a5d33bf77283ebf00b62420f3968230cca61be
George Ma <gma@seek.com.au>
2016-03-27 09:49:20 +1100

    Add tracking

--- app/services/auth.js
+++ app/services/auth.js
@@ -2,7 +2,7 @@
 
 var config = require('../app.constant');
 
-module.exports = ['$q', '$http', '$rootScope', 'AUTH_EVENTS', 'TestService', function ($q, $http, $rootScope, AUTH_EVENTS, TestService) {
+module.exports = ['$q', '$http', '$rootScope', '$cookies', 'AUTH_EVENTS', 'TestService', function ($q, $http, $rootScope, $cookies, AUTH_EVENTS, TestService) {
   var currentUser;
 
   /**
@@ -42,19 +42,43 @@ module.exports = ['$q', '$http', '$rootScope', 'AUTH_EVENTS', 'TestService', fun
       return $q.reject(res.data && res.data.displayMessage || 'Server error, please try again')
     });
   };
+
   /**
    * Check if a user has logged in
    * @returns {*}
    */
   var isAuthenticated = function() {
+    if (!$cookies.get('GT_VISITOR')) {
+      $cookies.put('GT_VISITOR', (Math.random() + 1).toString(36).substr(2, 10), {
+        path: '/',
+        domain: '.gotute.com',
+        expires: '9999-12-31T23:59:59.000Z'
+      })
+    }
+
     if (angular.isDefined(currentUser)) {
+      if (!$cookies.get('GT_USER')) {
+        if (currentUser) {
+          $cookies.put('GT_USER', currentUser.id, {
+            path: '/',
+            domain: '.gotute.com',
+            expires: '9999-12-31T23:59:59.000Z'
+          })
+        } else $cookies.remove('GT_USER');
+      }
       return currentUser ? $q.resolve(currentUser) : $q.reject();
     } else {
       var url = config.TUTOR_API + 'users/me';
       return $http.get(url).then(function(response) {
         currentUser = response.data;
+        $cookies.put('GT_USER', currentUser.id, {
+          path: '/',
+          domain: '.gotute.com',
+          expires: '9999-12-31T23:59:59.000Z'
+        });
         return currentUser;
       }, function(e) {
+        $cookies.remove('GT_USER');
         return $q.reject(e);
       });
     }
